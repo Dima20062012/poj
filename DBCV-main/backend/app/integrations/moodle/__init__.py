83e7c5279b9f39f5ca1ac008c716d5e471fe0591
ice2019warrior <dimon.2006.fed@gmail.com>
2026-01-06 23:19:21 +0300

    fix moodle

--- DBCV-main/backend/app/integrations/moodle/__init__.py
+++ DBCV-main/backend/app/integrations/moodle/__init__.py
@@ -1,4 +1,5 @@
-from .get_courses import MoodleGetCoursesIntegration
+# Если файл назывался get_courses, удали старый импорт
+from .create_course import MoodleCreateCourseIntegration
 from app.integrations.registry import registry
 
-registry.register(MoodleGetCoursesIntegration())
+registry.register(MoodleCreateCourseIntegration())
